6015fbb5c1f67ea55c44a704e9458c2fcddb8beb
William Casarin <jb55@jb55.com>
2024-06-17 12:46:08 -0700

    use less aggressive fade
    Signed-off-by: William Casarin <jb55@jb55.com>

--- egui-nav/src/lib.rs
+++ egui-nav/src/lib.rs
@@ -336,12 +336,8 @@ impl<T: Clone> Nav<T> {
 
             state.popped_min_rect = Some(ui.min_rect());
 
-            let alpha = ((1.0 - (state.offset / available_rect.width())) * 180.0) as u8;
-            let fade_color = if ui.visuals().dark_mode {
-                egui::Color32::from_black_alpha(alpha)
-            } else {
-                egui::Color32::from_white_alpha(alpha)
-            };
+            let alpha = ((1.0 - (state.offset / available_rect.width())) * 127.0) as u8;
+            let fade_color = egui::Color32::from_black_alpha(alpha);
 
             ui.painter()
                 .rect_filled(clip, egui::Rounding::default(), fade_color);
