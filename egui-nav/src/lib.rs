4efb1756d250ac37e16152d77503a1163fc9c13c
kernelkind <kernelkind@gmail.com>
2025-05-22 16:19:03 -0400

    extract shared navigation logic
    in preperation for popup sheet
    
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- egui-nav/src/lib.rs
+++ egui-nav/src/lib.rs
@@ -46,6 +46,63 @@ impl NavAction {
             NavAction::Navigating => true,
         }
     }
+
+    fn handle(
+        self,
+        ui: &mut egui::Ui,
+        state: &mut State,
+        drag_direction: DragDirection,
+        offset_at_rest: f32,
+        max_size: f32,
+    ) {
+        match self {
+            NavAction::Dragging => {
+                state.offset += drag_delta(ui, drag_direction);
+                if state.offset < 0.0 {
+                    state.offset = 0.0;
+                }
+            }
+            NavAction::Returned => {
+                state.action = None;
+                state.offset = offset_at_rest;
+            }
+            NavAction::Navigated => {
+                state.action = None;
+            }
+            NavAction::Navigating => {
+                if let Some(offset) = spring_animate(state.offset, offset_at_rest, true) {
+                    ui.ctx().request_repaint();
+                    state.offset = offset;
+                } else {
+                    state.action = Some(NavAction::Navigated);
+                }
+            }
+            NavAction::Returning => {
+                // We're returning, move the current view off to the
+                // right until the entire view is gone.
+
+                if let Some(offset) = spring_animate(state.offset, max_size, false) {
+                    ui.ctx().request_repaint();
+                    state.offset = offset;
+                } else {
+                    state.offset = max_size;
+                    state.action = Some(NavAction::Returned);
+                }
+            }
+            NavAction::Resetting => {
+                // If we're resetting, animate the current offset
+                // back to the current view
+
+                let left = state.offset > offset_at_rest;
+                if let Some(offset) = spring_animate(state.offset, offset_at_rest, left) {
+                    ui.ctx().request_repaint();
+                    state.offset = offset;
+                } else {
+                    state.action = None
+                }
+            }
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug, Default)]
@@ -159,23 +216,14 @@ impl<'a, Route: Clone> Nav<'a, Route> {
 
         // We only handle dragging when there is more than 1 route
         if self.route.len() > 1 {
-            // Drag contents to transition back.
-            // We must do this BEFORE adding content to the `Nav`,
-            // or we will steal input from the widgets we contain.
-            let available_rect = ui.available_rect_before_wrap();
-            let content_response = ui.interact(available_rect, id.with("drag"), Sense::drag());
-            if content_response.dragged() {
-                state.action = Some(NavAction::Dragging)
-            } else if content_response.drag_stopped() {
-                // we've stopped dragging, check to see if the offset is
-                // passed a certain point, to determine if we should return
-                // or animate back
-
-                if state.offset > available_rect.width() / 2.0 {
-                    state.action = Some(NavAction::Returning)
-                } else {
-                    state.action = Some(NavAction::Resetting)
-                }
+            let drag = Drag::new(
+                id,
+                DragDirection::Horizontal,
+                ui.available_rect_before_wrap(),
+                state.offset,
+            );
+            if let Some(action) = drag.handle(ui) {
+                state.action = Some(action);
             }
         }
 
@@ -194,53 +242,16 @@ impl<'a, Route: Clone> Nav<'a, Route> {
         }
 
         if let Some(action) = state.action {
-            match action {
-                NavAction::Dragging => {
-                    state.offset += ui.input(|input| input.pointer.delta()).x;
-                    if state.offset < 0.0 {
-                        state.offset = 0.0;
-                    }
-                }
-                NavAction::Returned => {
-                    state.action = None;
-                }
-                NavAction::Navigated => {
-                    state.action = None;
-                }
-                NavAction::Navigating => {
-                    if let Some(offset) = spring_animate(state.offset, 0.0, true) {
-                        ui.ctx().request_repaint();
-                        state.offset = offset;
-                    } else {
-                        state.action = Some(NavAction::Navigated);
-                    }
-                }
-                NavAction::Returning => {
-                    // We're returning, move the current view off to the
-                    // right until the entire view is gone.
-
-                    if let Some(offset) =
-                        spring_animate(state.offset, available_rect.width(), false)
-                    {
-                        ui.ctx().request_repaint();
-                        state.offset = offset;
-                    } else {
-                        state.offset = 0.0;
-                        state.action = Some(NavAction::Returned);
-                    }
-                }
-                NavAction::Resetting => {
-                    // If we're resetting, animate the current offset
-                    // back to the current view
-
-                    if let Some(offset) = spring_animate(state.offset, 0.0, true) {
-                        ui.ctx().request_repaint();
-                        state.offset = offset;
-                    } else {
-                        state.action = None
-                    }
-                }
-            }
+            action.handle(
+                ui,
+                &mut state,
+                DragDirection::Horizontal,
+                0.0,
+                available_rect.width(),
+            );
+        }
+        if matches!(state.action, Some(NavAction::Returned)) {
+            state.offset = 0.0;
         }
 
         state.store(ui.ctx(), id);
@@ -249,69 +260,39 @@ impl<'a, Route: Clone> Nav<'a, Route> {
         // behind transition layer
         let transitioning = state.is_transitioning();
         if transitioning {
-            let id = ui.id().with("bg");
-            let min_rect = state.popped_min_rect.unwrap_or(available_rect);
-            let initial_shift = -min_rect.width() * 0.1;
-            let mut amt = initial_shift + springy(state.offset);
-            if amt > 0.0 {
-                amt = 0.0;
-            }
+            let x_translate_amt = {
+                let min_rect = state.popped_min_rect.unwrap_or(available_rect);
+                let initial_shift = -min_rect.width() * 0.1;
+                let mut amt = initial_shift + springy(state.offset);
+                if amt > 0.0 {
+                    amt = 0.0;
+                }
 
-            //let clip_width = state.offset.max(available_rect.width());
+                amt
+            };
+
             let clip = Rect::from_min_size(
-                available_rect.min + egui::vec2(-amt, 0.0),
+                available_rect.min + egui::vec2(-x_translate_amt, 0.0),
                 vec2(state.offset, available_rect.max.y),
             );
 
-            let layer_id = LayerId::new(Order::Background, id);
-            let mut ui = egui::Ui::new(
-                ui.ctx().clone(),
-                ui.id(),
-                egui::UiBuilder::new()
-                    .layer_id(layer_id)
-                    .max_rect(available_rect),
-            );
-            ui.set_clip_rect(clip);
-
-            // render the previous nav view in the background when
-            // transitioning
-            let nav = Nav {
+            let translate_vec = egui::vec2(x_translate_amt, 0.0);
+            let bg_nav = Nav {
                 route: &self.route[..self.route.len() - 1],
                 ..*self
             };
-            let _r = show_route(&mut ui, NavUiType::Body, &nav);
-
-            state.popped_min_rect = Some(ui.min_rect());
 
             let strength = 50.0; // fade strength (max is 255)
             let alpha = ((1.0 - (state.offset / available_rect.width())) * strength) as u8;
-            let fade_color = egui::Color32::from_black_alpha(alpha);
+            let min_rect = render_bg(ui, Some(translate_vec), clip, available_rect, alpha, |ui| {
+                show_route(ui, NavUiType::Body, &bg_nav);
+            });
 
-            ui.painter()
-                .rect_filled(clip, egui::CornerRadius::default(), fade_color);
-
-            if amt < 0.0 {
-                ui.ctx().transform_layer_shapes(
-                    ui.layer_id(),
-                    TSTransform::from_translation(Vec2::new(amt, 0.0)),
-                );
-            }
+            state.popped_min_rect = Some(min_rect);
         }
 
         // foreground layer
         {
-            let layer_id = if transitioning {
-                // when transitioning, we need a new layer id otherwise the
-                // view transform will transform more things than we want
-                LayerId::new(Order::Foreground, id)
-            } else {
-                // if we don't use the same layer id as the ui, then we
-                // will have scrollview MouseWheel scroll issues due to
-                // the way rect_contains_pointer works with overlapping
-                // layers
-                ui.layer_id()
-            };
-
             let clip = Rect::from_min_size(
                 available_rect.min,
                 vec2(
@@ -320,37 +301,30 @@ impl<'a, Route: Clone> Nav<'a, Route> {
                 ),
             );
 
-            let mut ui = egui::Ui::new(
-                ui.ctx().clone(),
-                ui.id(),
-                egui::UiBuilder::new()
-                    .layer_id(layer_id)
-                    .max_rect(available_rect),
+            let response = render_fg(
+                ui,
+                transitioning,
+                Some(Vec2::new(state.offset, 0.0)),
+                clip,
+                available_rect,
+                |ui| {
+                    if let Some(NavAction::Returned) = state.action {
+                        // to avoid a flicker, render the popped route when we
+                        // are in the returned state
+                        let nav = Nav {
+                            route: &self.route[..self.route.len() - 1],
+                            ..*self
+                        };
+                        show_route(ui, NavUiType::Body, &nav)
+                    } else {
+                        show_route(ui, NavUiType::Body, self)
+                    }
+                },
             );
-            ui.set_clip_rect(clip);
-
-            let response = if let Some(NavAction::Returned) = state.action {
-                // to avoid a flicker, render the popped route when we
-                // are in the returned state
-                let nav = Nav {
-                    route: &self.route[..self.route.len() - 1],
-                    ..*self
-                };
-                show_route(&mut ui, NavUiType::Body, &nav)
-            } else {
-                show_route(&mut ui, NavUiType::Body, self)
-            };
-
-            if state.offset != 0.0 {
-                ui.ctx().transform_layer_shapes(
-                    ui.layer_id(),
-                    egui::emath::TSTransform::from_translation(Vec2::new(state.offset, 0.0)),
-                );
-            }
 
             NavResponse {
-                title_response,
                 response,
+                title_response: title_response,
                 action: state.action,
             }
         }
@@ -362,8 +336,15 @@ fn springy(offset: f32) -> f32 {
 }
 
 fn spring_animate(offset: f32, target: f32, left: bool) -> Option<f32> {
+    // nothing left to animate, user released drag beyond target
+    if (left && offset <= target) || (!left && offset >= target) {
+        return None;
+    }
+
     let abs_offset = (offset - target).abs();
-    if abs_offset > 0.0 {
+    if abs_offset > 0.1 {
+        // need some margin of error
+        // some margin of error is needed
         let sgn = (offset - target).signum();
         let amt = springy(abs_offset);
         let adj = amt * (if left { -1.0 } else { 1.0 });
@@ -379,4 +360,159 @@ fn spring_animate(offset: f32, target: f32, left: bool) -> Option<f32> {
         // we've reset, we're not in any specific state anymore
         None
     }
+}
+
+enum DragDirection {
+    Horizontal,
+    Vertical,
+}
+
+struct Drag {
+    id: egui::Id,
+    content_rect: egui::Rect,
+    direction: DragDirection,
+    offset_from_rest: f32,
+}
+
+impl Drag {
+    pub(crate) fn new(
+        parent_id: egui::Id,
+        direction: DragDirection,
+        content_rect: egui::Rect,
+        offset_from_rest: f32,
+    ) -> Self {
+        Drag {
+            id: parent_id.with("drag"),
+            content_rect,
+            direction,
+            offset_from_rest,
+        }
+    }
+
+    fn content_size(&self) -> f32 {
+        match self.direction {
+            DragDirection::Horizontal => self.content_rect.width(),
+            DragDirection::Vertical => self.content_rect.height(),
+        }
+    }
+
+    pub(crate) fn handle(self, ui: &mut egui::Ui) -> Option<NavAction> {
+        // Drag contents to transition back.
+        // We must do this BEFORE adding content to the `Nav`,
+        // or we will steal input from the widgets we contain.
+        let content_response = ui.interact(self.content_rect, self.id, Sense::drag());
+
+        if content_response.dragged() {
+            return Some(NavAction::Dragging);
+        } else if content_response.drag_stopped() {
+            // we've stopped dragging, check to see if the offset is
+            // passed a certain point, to determine if we should return
+            // or animate back
+
+            if self.offset_from_rest > self.content_size() / 2.0 {
+                return Some(NavAction::Returning);
+            } else {
+                return Some(NavAction::Resetting);
+            }
+        }
+
+        None
+    }
+}
+
+pub(crate) fn drag_delta(ui: &mut egui::Ui, direction: DragDirection) -> f32 {
+    match direction {
+        DragDirection::Horizontal => ui.input(|input| input.pointer.delta()).x,
+        DragDirection::Vertical => ui.input(|input| input.pointer.delta()).y,
+    }
+}
+
+pub(crate) fn render_bg(
+    ui: &mut egui::Ui,
+    translate_vec: Option<egui::Vec2>, // whether to translate the rendered route
+    clip: egui::Rect,                  // rect that should be clipped
+    available_rect: egui::Rect,        // rect of viewing area
+    alpha: u8,
+    mut render_route: impl FnMut(&mut egui::Ui),
+) -> egui::Rect {
+    let id = ui.id().with("bg");
+
+    let layer_id = LayerId::new(Order::Background, id);
+    let mut ui = egui::Ui::new(
+        ui.ctx().clone(),
+        id,
+        egui::UiBuilder::new()
+            .layer_id(layer_id)
+            .max_rect(available_rect),
+    );
+    ui.set_clip_rect(clip);
+
+    render_route(&mut ui);
+
+    let res = ui.min_rect();
+
+    let fade_color = egui::Color32::from_black_alpha(alpha);
+
+    ui.painter()
+        .rect_filled(clip, egui::CornerRadius::default(), fade_color);
+
+    let Some(translate_vec) = translate_vec else {
+        return res;
+    };
+
+    if translate_vec == Vec2::ZERO {
+        return res;
+    }
+
+    ui.ctx()
+        .transform_layer_shapes(ui.layer_id(), TSTransform::from_translation(translate_vec));
+
+    res
+}
+
+pub(crate) fn render_fg<R>(
+    ui: &mut egui::Ui,
+    transitioning: bool,
+    translate_vec: Option<egui::Vec2>, // whether to translate the rendered route
+    clip: egui::Rect,
+    available_rect: egui::Rect,
+    mut render_route: impl FnMut(&mut egui::Ui) -> R,
+) -> R {
+    let layer_id = if transitioning {
+        // when transitioning, we need a new layer id otherwise the
+        // view transform will transform more things than we want
+        LayerId::new(Order::Foreground, ui.id().with("fg"))
+    } else {
+        // if we don't use the same layer id as the ui, then we
+        // will have scrollview MouseWheel scroll issues due to
+        // the way rect_contains_pointer works with overlapping
+        // layers
+        ui.layer_id()
+    };
+
+    let mut ui = egui::Ui::new(
+        ui.ctx().clone(),
+        ui.id(),
+        egui::UiBuilder::new()
+            .layer_id(layer_id)
+            .max_rect(available_rect),
+    );
+    ui.set_clip_rect(clip);
+
+    let res = render_route(&mut ui);
+
+    let Some(translate_vec) = translate_vec else {
+        return res;
+    };
+
+    if translate_vec == Vec2::ZERO {
+        return res;
+    }
+
+    ui.ctx().transform_layer_shapes(
+        ui.layer_id(),
+        egui::emath::TSTransform::from_translation(translate_vec),
+    );
+
+    res
 }
